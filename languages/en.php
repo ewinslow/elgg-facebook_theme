eb65aced4562894698a9c557cb8ee5ab9a780908
Evan Winslow <evan@elgg.org>
2011-04-26 21:58:05 -0700

    Group menu items fixes (Why does Elgg prefix those with "Group" in the first place?

--- languages/en.php
+++ languages/en.php
@@ -1,1 +1,10 @@
 <?php
+add_translation('en', array(
+	'file:group' => "Files",
+	'bookmarks:group' => 'Links',
+	'blog:group' => "Blog",
+	'pages:group' => 'Pages',
+	'questions:group' => "Questions",
+	'discussion:group' => 'Discussion',
+	'groups:activity' => "Activity",
+));
