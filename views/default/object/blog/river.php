4f00fa998cc345a78944f095c9e2dbf44aa30f0c
Evan Winslow <evan@elgg.org>
2011-05-05 11:07:17 -0700

    Customized blog river appearance.

---
+++ views/default/object/blog/river.php
@@ -0,0 +1,16 @@
+<?php
+
+$blog = $vars['entity'];
+
+$title = elgg_view('output/url', array(
+	'text' => $blog->title,
+	'href' => $blog->getURL(),
+	'encode_text' => true,
+));
+
+$description = elgg_get_excerpt($blog->description, 350);
+
+echo elgg_view('river/elements/attachment', array(
+	'title' => $title,
+	'description' => $description,
+));
---
+++ views/default/river/object/blog/create.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Blog river view.
+ */
+
+$object = $vars['item']->getObjectEntity();
+$subject = $vars['item']->getSubjectEntity();
+
+$excerpt = strip_tags($object->excerpt);
+$excerpt = elgg_get_excerpt($excerpt);
+
+$link = elgg_view('output/url', array(
+	'href' => $object->getURL(),
+	'text' => $object->title,
+));
+
+$subject_link = elgg_view('output/url', array(
+	'href' => $subject->getURL(),
+	'text' => $subject->name,
+	'class' => 'elgg-river-subject',
+));
+
+$group_string = '';
+$container = $object->getContainerEntity();
+if ($container instanceof ElggGroup && elgg_get_page_owner_guid() != $container->guid) {
+	$group_link = elgg_view('output/url', array(
+		'href' => $container->getURL(),
+		'text' => $container->name,
+		'class' => 'elgg-river-target',
+	));
+	$group_string = "> $group_link";
+}
+
+echo elgg_view('river/elements/body', array(
+	'summary' => "$subject_link $group_string",
+	'content' => $excerpt,
+	'attachments' => elgg_view('object/blog/river', array('entity' => $object)),
+));
